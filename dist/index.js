7f05643b4e1f2ab93ac029dc9c16d1cb415103d9
J. Hite Billes <hite@billes.io>
2017-03-30 12:57:00 -0500

    First commit

---
+++ dist/index.js
@@ -0,0 +1,60 @@
+'use strict';
+
+var _dist = require('utility/dist');
+
+function _toConsumableArray(arr) { if (Array.isArray(arr)) { for (var i = 0, arr2 = Array(arr.length); i < arr.length; i++) { arr2[i] = arr[i]; } return arr2; } else { return Array.from(arr); } }
+
+var Velocity = require('velocity');
+
+
+var Toggle = function Toggle(_ref) {
+	var _ref$toggleSelector = _ref.toggleSelector,
+	    toggleSelector = _ref$toggleSelector === undefined ? '[data-toggle]' : _ref$toggleSelector,
+	    _ref$parentClass = _ref.parentClass,
+	    parentClass = _ref$parentClass === undefined ? null : _ref$parentClass,
+	    _ref$parentOpenClass = _ref.parentOpenClass,
+	    parentOpenClass = _ref$parentOpenClass === undefined ? null : _ref$parentOpenClass,
+	    _ref$targetClass = _ref.targetClass,
+	    targetClass = _ref$targetClass === undefined ? null : _ref$targetClass,
+	    _ref$closeAnimation = _ref.closeAnimation,
+	    closeAnimation = _ref$closeAnimation === undefined ? 'slideUp' : _ref$closeAnimation,
+	    _ref$openAnimation = _ref.openAnimation,
+	    openAnimation = _ref$openAnimation === undefined ? 'slideDown' : _ref$openAnimation,
+	    _ref$duration = _ref.duration,
+	    duration = _ref$duration === undefined ? 200 : _ref$duration;
+
+	var toggleNodes = document.querySelectorAll(toggleSelector);
+
+	if (toggleNodes.length) {
+		var toggles = [].concat(_toConsumableArray(toggleNodes));
+
+		toggles.map(function (toggle) {
+			parentClass = parentClass || toggle.getAttribute('data-toggle-parent-class');
+			var parent = parentClass !== null ? _dist.findParent.withClass(toggle, parentClass) : null;
+			if (!parent) return;
+
+			parentOpenClass = parentOpenClass || parentClass + '--open';
+			var target = parent.querySelector(targetClass || toggle.getAttribute('data-toggle'));
+			if (!target) return;
+
+			Velocity(target, closeAnimation, { duration: 0 });
+
+			toggle.addEventListener('click', function (e) {
+				e.preventDefault;
+
+				var animation = parent.classList.contains(parentOpenClass) ? closeAnimation : openAnimation;
+				var toggleAltText = toggle.getAttribute('data-toggle-alt-text') ? toggle.getAttribute('data-toggle-alt-text') : null;
+				var toggleText = toggle.innerHTML;
+
+				Velocity(target, animation, { duration: duration });
+				if (toggleAltText !== null) {
+					toggle.innerHTML = toggleAltText;
+					toggle.setAttribute('data-toggle-alt-text', toggleText);
+				}
+				parent.classList.toggle(parentOpenClass);
+			}, false);
+		});
+	}
+};
+
+module.exports = Toggle;
---
+++ src/index.js
@@ -0,0 +1,47 @@
+const Velocity = require('velocity')
+import { findParent } from 'utility/dist'
+
+const Toggle = ({
+	toggleSelector = '[data-toggle]',
+	parentClass = null,
+	parentOpenClass = null,
+	targetClass = null,
+	closeAnimation = 'slideUp',
+	openAnimation = 'slideDown',
+	duration = 200
+	}) => {
+	const toggleNodes = document.querySelectorAll(toggleSelector)
+
+	if (toggleNodes.length) {
+		const toggles = [...toggleNodes]
+
+		toggles.map(toggle => {
+			parentClass = parentClass || toggle.getAttribute('data-toggle-parent-class')
+			const parent = parentClass !== null ? findParent.withClass(toggle, parentClass) : null
+			if (!parent) return
+
+			parentOpenClass = parentOpenClass || `${parentClass}--open`
+			const target = parent.querySelector(targetClass || toggle.getAttribute('data-toggle'))
+			if (!target) return
+
+			Velocity(target, closeAnimation, { duration: 0 })
+
+			toggle.addEventListener('click', e => {
+				e.preventDefault
+
+				const animation = parent.classList.contains(parentOpenClass) ? closeAnimation : openAnimation
+				const toggleAltText = toggle.getAttribute('data-toggle-alt-text') ? toggle.getAttribute('data-toggle-alt-text') : null
+				const toggleText = toggle.innerHTML
+
+				Velocity(target, animation, { duration })
+				if (toggleAltText !== null) {
+					toggle.innerHTML = toggleAltText
+					toggle.setAttribute('data-toggle-alt-text', toggleText)
+				}
+				parent.classList.toggle(parentOpenClass)
+			}, false)
+		})
+	}
+}
+
+module.exports = Toggle
